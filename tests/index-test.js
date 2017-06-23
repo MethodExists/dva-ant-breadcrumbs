64f05ebb8c0934ee029ec127f8ec9f3ba437b780
ilya <ilya@methodexists.com>
2017-06-23 10:48:24 +0200

    Setup tests

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,23 +1,33 @@
-import expect from 'expect'
-import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
+import expect from 'expect';
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import configureStore from 'redux-mock-store';
+import Component from 'src/';
 
-import Component from 'src/'
+const mockStore = configureStore();
+const store = mockStore();
+const Br = props => <Component store={store} {...props} />;
 
 describe('Component', () => {
-  let node
+  let node;
 
   beforeEach(() => {
-    node = document.createElement('div')
-  })
+    node = document.createElement('div');
+  });
 
   afterEach(() => {
-    unmountComponentAtNode(node)
-  })
+    unmountComponentAtNode(node);
+  });
 
-  it('displays a welcome message', () => {
-    render(<Component/>, node, () => {
-      expect(node.innerHTML).toContain('Welcome to React components')
-    })
-  })
-})
+  it('should render the component', () => {
+    render(<Br routes={[]} params={{}} />, node, () => {
+      expect(node.innerHTML).toContain('ant-breadcrumb');
+    });
+  });
+
+  it('should use custom classname', () => {
+    render(<Br routes={[]} params={{}} className="customClass" />, node, () => {
+      expect(node.innerHTML).toContain('customClass');
+    });
+  });
+});
